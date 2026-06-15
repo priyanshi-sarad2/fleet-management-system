2de5efd30db40345d827233c7448c3d5417d45fb
Priyanshi Sarad <itspriyanshisarad@gmail.com>
2026-06-15 15:39:27 +0530

    modified

--- Infrastructure/modules/k8s-modules/k8s-namespace/service-account/main.tf
+++ Infrastructure/modules/k8s-modules/k8s-namespace/service-account/main.tf
@@ -1,6 +1,6 @@
 ###   Kubernetes Service Account   ###
 
-resource "kubernetes_service_account" "service_account" {
+resource "kubernetes_service_account_v1" "service_account" {
   count = var.create ? 1 : 0
   metadata {
     name        = var.service_account_name
--- Infrastructure/modules/k8s-modules/k8s-namespace/service-account/output.tf
+++
@@ -1,4 +0,0 @@
-output "service_account_name" {
-  description = "The name of the created service account"
-  value       = try(kubernetes_service_account.service_account[0].metadata[0].name, null)
-}
